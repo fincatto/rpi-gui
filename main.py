6b226210f9d1e3f5f3fb7131a20f4af440be900f
Diego Fincatto <diego@fincatto.com>
2020-02-29 16:02:36 -0300

    Extraida janela para classe extensora.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import sys
 import tkinter as tk
+from tkinter import ttk
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.config(cursor="none")
+        self.title("Imagem Filmes")
+        self.bind('<Escape>', sys.exit)
+        self.attributes("-fullscreen", True)
+
+        # label aleatorio
+        lbl = tk.Label(self, text="Oi mundo!")
+        lbl.grid(sticky="W", column=0, row=0)
+
+        # barra de progresso aleatoria
+        pb = ttk.Progressbar(self, orient="horizontal", length=200, mode="determinate")
+        pb.grid(sticky="W", column=0, row=1)
+        pb["maximum"] = 100
+        pb["value"] = 10
+
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.config(cursor="none")
-    window.title("Imagem Filmes")
-    window.bind('<Escape>', sys.exit)
-    window.attributes("-fullscreen", True)
-    lbl = tk.Label(window, text="Oi mundo!")
-    lbl.grid(column=0, row=0)
+    window = MainWindow()
     window.mainloop()
